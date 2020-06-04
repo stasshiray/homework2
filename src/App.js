6b300985b7abbdc14cf98efaff93d40ca7d30ff5
Stas Shiray <stasshiray@gmail.com>
2020-06-04 20:38:58 +0300

    add onClick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <button>Click</button>
+        <button onClick={() => {}}>Click</button>
       </header>
     </div>
   );
